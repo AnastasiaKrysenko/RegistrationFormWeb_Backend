c27577a6c6d1b82e3117ca8ea6fd8544343d81ef
AnastasiaKrysenko <110193588+AnastasiaKrysenko@users.noreply.github.com>
2023-04-03 22:09:31 +0300

    Add files via upload

---
+++ src/test/java/com/example/registrationformweb/RegistrationFormWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.registrationformweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationFormWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
